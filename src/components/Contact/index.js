674bbcc0281faa35c27e6f5c895615d17d6845a1
Rizvan Sk <sk.rizvan4545@gmail.com>
2024-09-15 19:02:26 +0530

    installing dependencies

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_pdvj0hr', 'template_gsbrhl2', form.current, '_-WJGivSuTCxVSlLx')
       .then((result) => {
         setOpen(true);
         form.current.reset();
